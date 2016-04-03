e1b0e883444f1683c587db8a74b6c795096b624e
Rob MacDonell <rob@continualdesign.com>
2016-04-03 13:41:24 -0700

    updated splash screens

---
+++ hooks/after_prepare/copy_splash.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+// This will copy all of our assets (Icons, splash screens, etc.) into their appropriate places when we build our app.
+// Modify your source dir as needed.
+
+var ncp = require('ncp').ncp,
+    transfers = [
+        {
+            'source': './build/images/splash/ios',
+            'destination': './platforms/ios/MobileLoop/Resources/splash'
+        }
+    ];
+
+ncp.limit = 16;
+
+transfers.forEach(function(transfer) {
+    ncp(transfer.source, transfer.destination, function (err) {
+        if (err) {
+            return console.error(err);
+        }
+        console.log('====== Assets moved from ' + transfer.source + ' to ' + transfer.destination + ' ======');
+    });
+});
