3c96a95275fcac10afff233a39b89051ece26cf4
Rob MacDonell <rob@schoolloop.com>
2016-08-28 15:00:22 -0700

    add student fixes

--- build/js/add-student.js
+++ build/js/add-student.js
@@ -4,8 +4,6 @@
     angular.module('mobileloop')
         .controller('AddStudentController', ['$rootScope', '$scope', 'StorageService', 'DataService', 'StatusService',
             'gettextCatalog', AddStudentController])
-        .controller('ChangeSchoolController', ['$scope', '$timeout', 'LoginService', 'StorageService',
-            'SchoolService', 'gettextCatalog', ChangeSchoolController])
     ;
 
     function AddStudentController($rootScope, $scope, storageService, dataService, statusService, gettextCatalog) {
@@ -39,7 +37,6 @@
             var student;
             var numStudents = page.students.length;
             if(numStudents > 0) {
-                page.modalShowing = false;
                 var school = page.school;
                 student = page.students[0];
                 storageService.addStudents(school, page.students, true);
@@ -120,20 +117,10 @@
         };
 
         page.changeSchool = function() {
-            $rootScope.$broadcast("refresh.modal");
-            $scope.schoolModal.show();
-            page.modalShowing = true;
-        };
-
-        page.cancel = function() {
-            page.modalShowing = false;
-            $scope.schoolModal.hide();
+            $scope.mainNavigator.pushPage("change-school.html", {animation: 'slide', addCallback: page.addSchool});
         };
 
         page.addSchool = function(school, userLogin, password) {
-            $scope.schoolModal.hide();
-            page.modalShowing = false;
-
             page.school = school;
 
             storageService.addDomain(school, userLogin, password);
@@ -167,177 +154,5 @@
             page.error.initialLast = "";
         }
 
-    }
-
-    function ChangeSchoolController($scope, $timeout, loginService, storageService, schoolService, gettextCatalog) {
-        var modal = this;
-
-        var domain = storageService.getDefaultDomain();
-        modal.username = domain.user.userName;
-        modal.password = domain.user.hashedPassword;
-
-        modal.selectedSchool = "";
-        modal.searchParam = "";
-        modal.authFailed = false;
-
-        modal.placeholder = {};
-        modal.placeholder.username = gettextCatalog.getString("User Name");
-        modal.placeholder.password = gettextCatalog.getString("Password");
-        modal.placeholder.school = gettextCatalog.getString("School Name");
-
-        modal.error = {};
-        clearErrors();
-
-        modal.lookup = {};
-        modal.lookup.expanded = false;
-        modal.lookup.currentPage = 0;
-        modal.lookup.itemsPerPage = 20;
-        modal.lookup.displaySet = [];
-
-        modal.schoolList = [];
-
-        schoolService.list().then(
-            function(data) {
-                modal.schoolList = data;
-            }
-        );
-
-        $scope.$on("refresh.modal", function() {
-            modal.selectedSchool = "";
-            modal.searchParam = "";
-            modal.authFailed = false;
-        });
-
-        modal.isSchoolSelected = function () {
-            return isSchoolDefined(modal.selectedSchool) === true;
-        };
-
-        modal.selectSchool = function(school) {
-            if(isSchoolDefined(school) === true) {
-                modal.selectedSchool = school;
-                modal.searchParam = school.name;
-
-                var $input = $(".lookup-input");
-                $timeout(function() {
-                    $input.blur();
-                });
-            } else {
-                modal.selectedSchool = undefined;
-                modal.searchParam = "";
-                modal.submitted = false;
-                modal.authFailed = false;
-            }
-            modal.lookup.expanded = false;
-        };
-
-        modal.expand = function() {
-            clearDisplaySet();
-            loadData();
-            modal.lookup.expanded = true;
-        };
-
-        modal.clear = function() {
-            modal.selectSchool();
-            clearErrors();
-        };
-
-        modal.showClear = function() {
-            return _.isUndefined(modal.selectedSchool) === false || isSearchDefined() || modal.lookup.expanded === true;
-        };
-
-        modal.change = function() {
-            modal.lookup.expanded = (modal.searchParam && modal.searchParam.length > 0);
-            clearDisplaySet();
-            loadData();
-        };
-
-        function loadData() {
-            if(_.isUndefined(modal.schoolList) === true) {
-                return;
-            }
-            var start = modal.lookup.currentPage * modal.lookup.itemsPerPage;
-            var end = start + modal.lookup.itemsPerPage;
-            var results = modal.schoolList;
-            var i, len, item, queryRegExp = null;
-            if(isSearchDefined()) {
-                results = [];
-                queryRegExp = RegExp(modal.searchParam, 'i'); //'i' -> case insensitive
-                for(i = start, len = modal.schoolList.length; i < len; i++) {
-                    item = modal.schoolList[i];
-                    if(item.name.match(queryRegExp) || item.districtName.match(queryRegExp)) {
-                        results.push(item);
-                    }
-                    if(results.length > end) {
-                        break;
-                    }
-                }
-            }
-            for(i = start, len = results.length; i < len && i < end; i++) {
-                item = results[i];
-                modal.lookup.displaySet.push(item);
-            }
-            if(modal.lookup.displaySet.length === 1) {
-                modal.selectSchool(modal.lookup.displaySet[0]);
-            } else if(modal.lookup.displaySet.length === 0) {
-                modal.selectedSchool = "";
-            }
-        }
-
-        modal.select = function() {
-            if(_.isUndefined(modal.selectedSchool) === false) {
-                clearErrors();
-                var newSchool = modal.selectedSchool;
-                loginService.login(newSchool.domainName, modal.username, modal.password).then(
-                    function(response) {
-                        processSuccess(response, newSchool, domain.user.hashedPassword);
-                    },
-                    function(error) {
-                        processFailure(error);
-                    }
-                );
-            }
-        };
-
-        function processSuccess(response, school, password) {
-            var data = response.data;
-            if (data.isUnverifiedParent === 'true') {
-                modal.message = gettextCatalog.getString("Parent needs to be verified");
-            } else {
-                $scope.page.addSchool(school, data, password);
-            }
-        }
-
-        function processFailure(error) {
-            var status = error.data;
-            var isCredError = status.toLowerCase().startsWith("error 1:") ||
-                status.toLowerCase().startsWith("error 2:");
-            if(isCredError === true) {
-                modal.authFailed = true;
-                modal.password = "";
-                modal.error.school =  gettextCatalog.getString("Invalid Login");
-            }
-            modal.message = "";
-            console.log(error);
-        }
-
-        function isSearchDefined() {
-            return _.isUndefined(modal.searchParam) === false && modal.searchParam.length > 0;
-        }
-
-        function clearDisplaySet() {
-            modal.lookup.displaySet = [];
-            modal.lookup.currentPage = 0;
-            //$('.lookup-content').scrollTop(0);
-        }
-
-        function clearErrors() {
-            modal.error.school = "";
-            modal.error.username = "";
-            modal.error.password = "";
-        }
-
-        function isSchoolDefined(school) {
-            return _.isUndefined(school) === false && _.isNull(school) === false && _.isUndefined(school.domainName) === false;
-        }
     }
 })();
---
+++ build/js/change-school.js
@@ -0,0 +1,183 @@
+(function() {
+    'use strict';
+
+    angular.module('mobileloop')
+        .controller('ChangeSchoolController', ['$scope', '$timeout', 'LoginService', 'StorageService',
+            'SchoolService', 'gettextCatalog', ChangeSchoolController])
+    ;
+
+    function ChangeSchoolController($scope, $timeout, loginService, storageService, schoolService, gettextCatalog) {
+        var page = this;
+
+        page.addCallback = $scope.mainNavigator.topPage.pushedOptions.addCallback;
+
+        var domain = storageService.getDefaultDomain();
+        page.username = domain.user.userName;
+        page.password = domain.user.hashedPassword;
+
+        page.selectedSchool = "";
+        page.searchParam = "";
+        page.authFailed = false;
+
+        page.placeholder = {};
+        page.placeholder.username = gettextCatalog.getString("User Name");
+        page.placeholder.password = gettextCatalog.getString("Password");
+        page.placeholder.school = gettextCatalog.getString("School Name");
+
+        page.error = {};
+        clearErrors();
+
+        page.lookup = {};
+        page.lookup.expanded = false;
+        page.lookup.currentPage = 0;
+        page.lookup.itemsPerPage = 20;
+        page.lookup.displaySet = [];
+
+        page.schoolList = [];
+
+        schoolService.list().then(
+            function(data) {
+                page.schoolList = data;
+            }
+        );
+
+        $scope.$on("refresh.modal", function() {
+            page.selectedSchool = "";
+            page.searchParam = "";
+            page.authFailed = false;
+        });
+
+        page.isSchoolSelected = function () {
+            return isSchoolDefined(page.selectedSchool) === true;
+        };
+
+        page.selectSchool = function(school) {
+            if(isSchoolDefined(school) === true) {
+                page.selectedSchool = school;
+                page.searchParam = school.name;
+
+                var $input = $(".lookup-input");
+                $timeout(function() {
+                    $input.blur();
+                }, 100);
+            } else {
+                page.selectedSchool = undefined;
+                page.searchParam = "";
+                page.submitted = false;
+                page.authFailed = false;
+            }
+            page.lookup.expanded = false;
+        };
+
+        page.expand = function() {
+            clearDisplaySet();
+            loadData();
+            page.lookup.expanded = true;
+        };
+
+        page.clear = function() {
+            page.selectSchool();
+            clearErrors();
+        };
+
+        page.showClear = function() {
+            return _.isUndefined(page.selectedSchool) === false || isSearchDefined() || page.lookup.expanded === true;
+        };
+
+        page.change = function() {
+            page.lookup.expanded = (page.searchParam && page.searchParam.length > 0);
+            clearDisplaySet();
+            loadData();
+        };
+
+        function loadData() {
+            if(_.isUndefined(page.schoolList) === true) {
+                return;
+            }
+            var start = page.lookup.currentPage * page.lookup.itemsPerPage;
+            var end = start + page.lookup.itemsPerPage;
+            var results = page.schoolList;
+            var i, len, item, queryRegExp = null;
+            if(isSearchDefined()) {
+                results = [];
+                queryRegExp = RegExp(page.searchParam, 'i'); //'i' -> case insensitive
+                for(i = start, len = page.schoolList.length; i < len; i++) {
+                    item = page.schoolList[i];
+                    if(item.name.match(queryRegExp) || item.districtName.match(queryRegExp)) {
+                        results.push(item);
+                    }
+                    if(results.length > end) {
+                        break;
+                    }
+                }
+            }
+            for(i = start, len = results.length; i < len && i < end; i++) {
+                item = results[i];
+                page.lookup.displaySet.push(item);
+            }
+            if(page.lookup.displaySet.length === 1) {
+                page.selectSchool(page.lookup.displaySet[0]);
+            } else if(page.lookup.displaySet.length === 0) {
+                page.selectedSchool = "";
+            }
+        }
+
+        page.select = function() {
+            if(_.isUndefined(page.selectedSchool) === false) {
+                clearErrors();
+                var newSchool = page.selectedSchool;
+                loginService.login(newSchool.domainName, page.username, page.password).then(
+                    function(response) {
+                        processSuccess(response, newSchool, domain.user.hashedPassword);
+                    },
+                    function(error) {
+                        processFailure(error);
+                    }
+                );
+            }
+        };
+
+        function processSuccess(response, school, password) {
+            var data = response.data;
+            if (data.isUnverifiedParent === 'true') {
+                page.message = gettextCatalog.getString("Parent needs to be verified");
+            } else {
+                $scope.mainNavigator.popPage();
+                page.addCallback(school, data, password);
+            }
+        }
+
+        function processFailure(error) {
+            var status = error.data;
+            var isCredError = status.toLowerCase().startsWith("error 1:") ||
+                status.toLowerCase().startsWith("error 2:");
+            if(isCredError === true) {
+                page.authFailed = true;
+                page.password = "";
+                page.error.school =  gettextCatalog.getString("Invalid Login");
+            }
+            page.message = "";
+            console.log(error);
+        }
+
+        function isSearchDefined() {
+            return _.isUndefined(page.searchParam) === false && page.searchParam.length > 0;
+        }
+
+        function clearDisplaySet() {
+            page.lookup.displaySet = [];
+            page.lookup.currentPage = 0;
+            //$('.lookup-content').scrollTop(0);
+        }
+
+        function clearErrors() {
+            page.error.school = "";
+            page.error.username = "";
+            page.error.password = "";
+        }
+
+        function isSchoolDefined(school) {
+            return _.isUndefined(school) === false && _.isNull(school) === false && _.isUndefined(school.domainName) === false;
+        }
+    }
+})();
