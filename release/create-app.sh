575b0c0651259ace0cb7d649897304bcd9f4ff28
Rob MacDonell <rob@continualdesign.com>
2018-04-17 20:48:40 -0700

    Fix for android push plugin issue

--- release/create-app.sh
+++ release/create-app.sh
@@ -20,7 +20,7 @@ if [ ! -d "app" ]; then
 #    cordova plugin add https://github.com/amritk/cordova-plugin-firebase
 
     cordova plugin add com.cmackay.plugins.googleanalytics --save
-    cordova plugin add phonegap-plugin-push@1.10.7 --variable SENDER_ID="497458348283" --save
+    cordova plugin add phonegap-plugin-push@1.11.1 --variable SENDER_ID="497458348283" --save
     cordova plugin add cordova-hot-code-push-plugin@1.5.3 --save
 
     rm -rf res/icon/*
@@ -32,7 +32,7 @@ if [ ! -d "app" ]; then
 
 #    cp -r build/custom/xcshareddata app/platforms/ios/MobileLoop.xcodeproj/
     cp build/custom/build.json app/
-    cp build/custom/PushPlugin.m app/platforms/ios/MobileLoop/Plugins/phonegap-plugin-push/PushPlugin.m
+#    cp build/custom/PushPlugin.m app/platforms/ios/MobileLoop/Plugins/phonegap-plugin-push/PushPlugin.m
     cp build/versions/.chcplogin app/
     cp build/versions/$APP_ID/cordova-hcp.json app/
     ./release/update-xcode.sh $APP_ID
